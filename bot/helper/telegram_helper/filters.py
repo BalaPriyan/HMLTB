b7bef146aa4e787b4a6aa8870cee01420c313131
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-07-24 08:28:18 -0700

    Update filters.py

--- bot/helper/telegram_helper/filters.py
+++ bot/helper/telegram_helper/filters.py
@@ -1,7 +1,7 @@
 from pyrogram.filters import create
-
+from pyrogram.enums import ChatType
 from bot import OWNER_ID, user_data
-
+from bot.helper.telegram_helper.message_utils import chat_info
 
 class CustomFilters:
 
@@ -21,8 +21,28 @@ class CustomFilters:
 
     authorized = create(authorized_user)
 
-    async def sudo_user(self, client, update):
-        user = update.from_user or update.sender_chat
+    async def authorized_usetting(self, _, message):
+        uid = (message.from_user or message.sender_chat).id
+        chat_id = message.chat.id
+        isExists = False
+        if uid == OWNER_ID or (uid in user_data and (user_data[uid].get('is_auth', False) or user_data[uid].get('is_sudo', False))) or (chat_id in user_data and user_data[chat_id].get('is_auth', False)):
+            isExists = True
+        elif message.chat.type == ChatType.PRIVATE:
+            for channel_id in user_data:
+                if not (user_data[channel_id].get('is_auth') and str(channel_id).startswith('-100')):
+                    continue
+                try:
+                    if await (await chat_info(str(channel_id))).get_member(uid):
+                        isExists = True
+                        break
+                except:
+                    continue
+        return isExists
+        
+    authorized_uset = create(authorized_usetting)
+
+    async def sudo_user(self, _, message):
+        user = message.from_user or message.sender_chat
         uid = user.id
         return bool(uid == OWNER_ID or uid in user_data and user_data[uid].get('is_sudo'))
 

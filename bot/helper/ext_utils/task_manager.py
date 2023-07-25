3e8cfca0e8964dfc6798fc4effcbfae5bed11fee
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-07-24 22:36:52 -0700

    Update task_manager.py

--- bot/helper/ext_utils/task_manager.py
+++ bot/helper/ext_utils/task_manager.py
@@ -1,10 +1,11 @@
 from asyncio import Event
 
-from bot import (LOGGER, config_dict, non_queued_dl, non_queued_up, queue_dict_lock, queued_dl, queued_up)
-from bot.helper.ext_utils.bot_utils import (get_readable_file_size, get_telegraph_list, sync_to_async)
+from bot import ( OWNER_ID, LOGGER, config_dict, non_queued_dl, non_queued_up, queue_dict_lock, queued_dl, queued_up, user_data, download_dict)
+from bot.helper.ext_utils.bot_utils import (get_readable_file_size, get_telegraph_list, sync_to_async,  get_user_tasks, getdailytasks, checking_access)
 from bot.helper.ext_utils.fs_utils import check_storage_threshold, get_base_name
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-
+from bot.helper.telegram_helper.message_utils import forcesub, BotPm_check, user_info
+from bot.helper.themes import BotTheme
 
 async def stop_duplicate_check(name, listener):
     if (
@@ -25,7 +26,7 @@ async def stop_duplicate_check(name, listener):
     if name is not None:
         telegraph_content, contents_no = await sync_to_async(GoogleDriveHelper().drive_list, name, stopDup=True)
         if telegraph_content:
-            msg = f"File/Folder is already available in Drive.\nHere are {contents_no} list results:"
+            msg = BotTheme('STOP_DUPLICATE', content=contents_no)
             button = await get_telegraph_list(telegraph_content)
             return msg, button
     return False, None
@@ -111,6 +112,10 @@ async def start_from_queued():
 
 
 async def limit_checker(size, listener, isTorrent=False, isMega=False, isDriveLink=False, isYtdlp=False):
+    LOGGER.info('Checking Size Limit of link/file/folder/tasks...')
+    user_id = listener.message.from_user.id
+    if user_id == OWNER_ID or user_id in user_data and user_data[user_id].get('is_sudo'):
+        return
     limit_exceeded = ''
     if listener.isClone:
         if CLONE_LIMIT := config_dict['CLONE_LIMIT']:
@@ -141,15 +146,69 @@ async def limit_checker(size, listener, isTorrent=False, isMega=False, isDriveLi
         limit = DIRECT_LIMIT * 1024**3
         if size > limit:
             limit_exceeded = f'Direct limit is {get_readable_file_size(limit)}'
-    if not limit_exceeded and (LEECH_LIMIT := config_dict['LEECH_LIMIT']) and listener.isLeech:
-        limit = LEECH_LIMIT * 1024**3
-        if size > limit:
-            limit_exceeded = f'Leech limit is {get_readable_file_size(limit)}'
-    if not limit_exceeded and (STORAGE_THRESHOLD := config_dict['STORAGE_THRESHOLD']) and not listener.isClone:
-        arch = any([listener.compress, listener.extract])
-        limit = STORAGE_THRESHOLD * 1024**3
-        acpt = await sync_to_async(check_storage_threshold, size, limit, arch)
-        if not acpt:
-            limit_exceeded = f'You must leave {get_readable_file_size(limit)} free storage.'
+
+    if not limit_exceeded:
+        if (LEECH_LIMIT := config_dict['LEECH_LIMIT']) and listener.isLeech:
+            limit = LEECH_LIMIT * 1024**3
+            if size > limit:
+                limit_exceeded = f'Leech limit is {get_readable_file_size(limit)}'
+        
+        if (STORAGE_THRESHOLD := config_dict['STORAGE_THRESHOLD']) and not listener.isClone:
+            arch = any([listener.compress, listener.extract])
+            limit = STORAGE_THRESHOLD * 1024**3
+            acpt = await sync_to_async(check_storage_threshold, size, limit, arch)
+            if not acpt:
+                limit_exceeded = f'You must leave {get_readable_file_size(limit)} free storage.'
+        
+        if (PLAYLIST_LIMIT := config_dict['PLAYLIST_LIMIT']):
+            limit_exceeded = f'Playlist limit is {PLAYLIST_LIMIT}'
+
+        if config_dict['DAILY_TASK_LIMIT'] and config_dict['DAILY_TASK_LIMIT'] <= await getdailytasks(user_id):
+            limit_exceeded = f"Daily Total Task Limit: {config_dict['DAILY_TASK_LIMIT']}\nYou have exhausted all your Daily Task Limits."
+        else:
+            ttask = await getdailytasks(user_id, increase_task=True)
+            LOGGER.info(f"User: {user_id} | Daily Tasks: {ttask}")
+        if (DAILY_MIRROR_LIMIT := config_dict['DAILY_MIRROR_LIMIT']) and not listener.isLeech:
+            limit = DAILY_MIRROR_LIMIT * 1024**3
+            if (size >= (limit - await getdailytasks(user_id, check_mirror=True)) or limit <= await getdailytasks(user_id, check_mirror=True)):
+                limit_exceeded = f'Daily Mirror Limit is {get_readable_file_size(limit)}\nYou have exhausted all your Daily Mirror Limit.'
+            elif not listener.isLeech:
+                msize = await getdailytasks(user_id, upmirror=size, check_mirror=True)
+                LOGGER.info(f"User : {user_id} | Daily Mirror Size : {get_readable_file_size(msize)}")
+        if (DAILY_LEECH_LIMIT := config_dict['DAILY_LEECH_LIMIT']) and listener.isLeech:
+            limit = DAILY_LEECH_LIMIT * 1024**3
+            if (size >= (limit - await getdailytasks(user_id, check_leech=True)) or limit <= await getdailytasks(user_id, check_leech=True)):
+                limit_exceeded = f'Daily Leech Limit is {get_readable_file_size(limit)}\nYou have exhausted all your Daily Leech Limit.'
+            elif listener.isLeech:
+                lsize = await getdailytasks(user_id, upleech=size, check_leech=True)
+                LOGGER.info(f"User : {user_id} | Daily Leech Size : {get_readable_file_size(lsize)}")
+
     if limit_exceeded:
         return f"{limit_exceeded}.\nYour File/Folder size is {get_readable_file_size(size)}"
+
+
+async def task_utils(message):
+    LOGGER.info('Running Task Manager ...')
+    msg = []
+    button = None
+
+    token_msg, button = checking_access(message.from_user.id, button)
+    if token_msg is not None:
+        msg.append(token_msg)
+    if message.chat.type != message.chat.type.BOT:
+        if ids := config_dict['FSUB_IDS']:
+            _msg, button = await forcesub(message, ids, button)
+            if _msg:
+                msg.append(_msg)
+        user_id = message.from_user.id
+        user_dict = user_data.get(user_id, {})
+        user = await user_info(message._client, message.from_user.id)
+        if config_dict['BOT_PM'] or user_dict.get('bot_pm'):
+            _msg, button = await BotPm_check(message, button)
+            if _msg:
+                msg.append(_msg)
+    if (bmax_tasks := config_dict['BOT_MAX_TASKS']) and len(download_dict) >= bmax_tasks:
+        msg.append(f"Bot Max Tasks limit exceeded.\nBot max tasks limit is {bmax_tasks}.\nPlease wait for the completion of other tasks.")
+    if (maxtask := config_dict['USER_MAX_TASKS']) and await get_user_tasks(message.from_user.id, maxtask):
+        msg.append(f"Your tasks limit exceeded for {maxtask} tasks")
+    return msg, button
